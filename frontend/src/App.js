da6a0bbae9c8b68622d06cfcdff9040150b66cf1
DevWhiz1 <139163026+DevWhiz1@users.noreply.github.com>
2025-10-24 22:51:50 +0500

    Change API base URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ import {
 } from 'react-icons/fi';
 import toast from 'react-hot-toast';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
+const API_BASE_URL = process.env.VITE_API_URL || 'https://source-code-lexer.vercel.app';
 
 function App() {
   const [results, setResults] = useState(null);
